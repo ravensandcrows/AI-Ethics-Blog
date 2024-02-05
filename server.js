2e525d779a2919b47c2a96e89a9109b7e1b06e36
ravensandcrows <blackstormkali@gmail.com>
2024-02-05 11:42:05 -0800

    created handlebars for join

--- server.js
+++ server.js
@@ -1,27 +1,51 @@
 // Dependencies
-const express = require('express');
 const path = require('path');
-const { engine } = require('express-handlebars');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
 const helper = require('./utils/helper');
+const routes = require('./controllers');
 
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Initialize and set up express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helper });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // Static middleware pointing to the public folder
-app.engine('handlebars', engine());
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views','./views');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //set up routes
 
-app.use(require('./controllers/homeRoutes'));
+app.use(routes);
 
 // listen() method is responsible for listening for incoming connections on the specified port 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
