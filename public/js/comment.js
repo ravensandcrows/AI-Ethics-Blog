6e7e7dff09ae6ffa3759b1414eecb700d6ab4b75
ravensandcrows <blackstormkali@gmail.com>
2024-02-06 01:13:22 -0800

    added public comment js

---
+++ public/js/comment.js
@@ -0,0 +1,28 @@
+const commentFormHandler = async function (event) {
+    event.preventDefault();
+    const postID = document.querySelector('input[name="post-id"]').value;
+    const body = document.querySelector('textarea[name="comment-body"]').value;
+  
+    if (body) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+          postID,
+          body,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } 
+      else {
+        document.location.replace('/login');
+      }
+    }
+  };
+  
+  document.querySelector('#new-comment-form').addEventListener('submit', commentFormHandler);
+  
