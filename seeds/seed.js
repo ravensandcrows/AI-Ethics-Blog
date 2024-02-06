2289eec66599df9fab5411f5a6de937e62bc1ba0
ravensandcrows <blackstormkali@gmail.com>
2024-02-05 23:38:03 -0800

    added handlebars for posts and cleaned up some blank lines in other files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,5 @@
 const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
-
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 
